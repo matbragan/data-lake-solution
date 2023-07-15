ba2646e7c5f120c9f5c4f84af6594401a1feed75
matbragan <matheus.bs.oliveira@gmail.com>
2023-07-15 13:12:36 -0300

    update data_extraction dag

---
+++ dags/data_extraction.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.decorators import task
+
+with DAG(
+    dag_id='extraction_date',
+    start_date=datetime(2023, 7, 15),
+    schedule='@daily',
+    catchup=False,
+    tags=['extraction']
+):
+
+    @task(task_id='mongo_extraction')
+    def mongo_extraction() -> None:
+        from extraction.mongo_extraction import mongo_connection
+        from extraction.utils.write_s3 import write_s3
+
+        data = mongo_connection('hotmart', 'users')
+        write_s3(data, 'users')
+
+    mongo_extraction()

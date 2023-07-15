182eb3c54bf9d67d6317fa7a9bcba4d27fb0dc74
matbragan <matheus.bs.oliveira@gmail.com>
2023-07-15 13:12:11 -0300

    update extractions

--- extraction/mysql_extraction.py
+++ extraction/mysql_extraction.py
@@ -2,29 +2,29 @@ import os
 
 import mysql.connector
 from dotenv import load_dotenv
-
 from utils.write_s3 import write_s3
 
 load_dotenv()
 
+
 def mysql_connection(database: str, table: str) -> list:
-  cnx = mysql.connector.connect(
-      host=os.getenv('MYSQL_CONN_HOST'),
-      user=os.getenv('MYSQL_CONN_USER'),
-      password=os.getenv('MYSQL_CONN_PASSWORD'),
-      database=database
-  )
+    cnx = mysql.connector.connect(
+        host=os.getenv('MYSQL_CONN_HOST'),
+        user=os.getenv('MYSQL_CONN_USER'),
+        password=os.getenv('MYSQL_CONN_PASSWORD'),
+        database=database
+    )
 
-  cursor = cnx.cursor()
+    cursor = cnx.cursor()
 
-  query = f'select * from {table};'
+    query = f'select * from {table};'
 
-  cursor.execute(query)
-  data = cursor.fetchall()
+    cursor.execute(query)
+    data = cursor.fetchall()
 
-  return data
+    return data
 
 
 if __name__ == '__main__':
-  data = mysql_connection('hotmart', 'sales')
-  write_s3(data, 'sales')
+    data = mysql_connection('hotmart', 'sales')
+    write_s3(data, 'sales')

ae463e777e5696d84552fbdb4ec613a6f77a9ea0
matbragan <matheus.bs.oliveira@gmail.com>
2023-08-06 16:30:35 -0300

    update aws secret key

--- airflow/dags/utils/write_s3.py
+++ airflow/dags/utils/write_s3.py
@@ -9,25 +9,29 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+aws_key = os.getenv('AWS_ACCESS_KEY_ID')
+aws_secret_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+
+
 def write_s3(
-    data: list, 
-    file_name: str, 
+    data: list,
+    file_name: str,
     bucket_name: str = 'hotmart-case'
 ) -> None:
 
-  df = pd.DataFrame(data)
-  table = pa.Table.from_pandas(df)
+    df = pd.DataFrame(data)
+    table = pa.Table.from_pandas(df)
 
-  buffer = io.BytesIO()
-  pq.write_table(table, buffer)
-  buffer.seek(0)
+    buffer = io.BytesIO()
+    pq.write_table(table, buffer)
+    buffer.seek(0)
 
-  s3 = boto3.client(
-      's3', 
-      aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-      aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
-  )
+    s3 = boto3.client(
+        's3',
+        aws_access_key_id=aws_key,
+        aws_secret_access_key=aws_secret_key
+    )
 
-  file_name = f'{file_name}.parquet'
+    file_name = f'{file_name}.parquet'
 
-  s3.upload_fileobj(buffer, bucket_name, file_name)
+    s3.upload_fileobj(buffer, bucket_name, file_name)
--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import os
+
+from dotenv import load_dotenv
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName('sparkSession123').getOrCreate()
+load_dotenv()
 
-print(spark)
+aws_key = os.getenv('AWS_ACCESS_KEY_ID')
+aws_secret_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+
+spark = SparkSession.builder.appName('read-s3').getOrCreate()
+
+spark.conf.set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.2.0')
+spark.conf.set('spark.hadoop.fs.s3a.access.key', aws_key)
+spark.conf.set('spark.hadoop.fs.s3a.secret.key', aws_secret_key)
+
+df = spark.read.parquet('s3a://hotmart-case/users.parquet')
+
+df.show()

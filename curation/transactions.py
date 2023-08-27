5b2ab94947dff6c5acd604360152b4ec3ba31140
matbragan <matheus.bs.oliveira@gmail.com>
2023-08-27 13:27:52 -0300

    curation layer with spark_writer

--- curation/transactions.py
+++ curation/transactions.py
@@ -1,9 +1,16 @@
+import sys
+
+sys.path.insert(1, '/home/matbragan/Documents/data-lake-solution/')
+
+from settings import LAYER
+
 from utils.spark_builder import SparkBuilder
+from utils.spark_writer import spark_writer
 
 spark = SparkBuilder().s3_connector()
 
-users = spark.read.load('s3a://lake-solution/extraction/users/')
-sales = spark.read.load('s3a://lake-solution/extraction/sales/')
+users = spark.read.load('s3a://lake-solution/raw/users/')
+sales = spark.read.load('s3a://lake-solution/raw/sales/')
 
 dataframe = (
     users
@@ -15,13 +22,10 @@ dataframe = (
     .select(
         users.id.alias('user_id'),
         users.name.alias('user_name'),
-        sales.id.alias('sale_id'),
-        sales.date.alias('sale_date'),
-        sales.value.alias('sale_value')
+        sales.id.alias('transaction_id'),
+        sales.date.alias('transaction_date'),
+        sales.value.alias('transaction_value')
     )
 )
 
-dataframe.repartition(1)\
-    .write.mode('overwrite')\
-    .partitionBy(None)\
-    .save(f's3a://lake-solution/curation/transactions/')
+spark_writer(dataframe=dataframe, layer=LAYER, table='transactions')

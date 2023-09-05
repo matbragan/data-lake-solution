f0da73eaf7aa691797726ac40f0a6c98caa62233
matbragan <matheus.bs.oliveira@gmail.com>
2023-09-04 23:57:29 -0300

    chore: add delta package and call method

--- utils/spark_builder.py
+++ utils/spark_builder.py
@@ -9,25 +9,31 @@ load_dotenv()
 class SparkBuilder():
 
     def __init__(self):
-        self.builder = SparkSession.builder.appName('data-lake-solution')
+        self.builder = SparkSession.builder.appName('data-lake-solution') \
+        .config('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.3.2,io.delta:delta-core_2.12:1.0.0')
+
+    def __call__(self):
+        self.s3_connector()
+        # self.delta_connector()
+        
+        return self.builder.getOrCreate()
 
     def s3_connector(self):
-        self.aws_key = os.getenv('AWS_ACCESS_KEY_ID')
-        self.aws_secret_key = os.getenv('AWS_SECRET_ACCESS_KEY')
-        self.aws_credentials_provider = 'SimpleAWSCredentialsProvider'
+        aws_key = os.getenv('AWS_ACCESS_KEY_ID')
+        aws_secret_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+        aws_credentials_provider = 'SimpleAWSCredentialsProvider'
         
         self.builder = (
             self.builder
-            .config('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.3.2')
             .config('spark.hadoop.fs.s3a.impl', 'org.apache.hadoop.fs.s3a.S3AFileSystem')
-            .config('spark.hadoop.fs.s3a.aws.credentials.provider', f'org.apache.hadoop.fs.s3a.{self.aws_credentials_provider}')
-            .config('spark.hadoop.fs.s3a.access.key', self.aws_key)
-            .config('spark.hadoop.fs.s3a.secret.key', self.aws_secret_key)
+            .config('spark.hadoop.fs.s3a.aws.credentials.provider', f'org.apache.hadoop.fs.s3a.{aws_credentials_provider}')
+            .config('spark.hadoop.fs.s3a.access.key', aws_key)
+            .config('spark.hadoop.fs.s3a.secret.key', aws_secret_key)
         )
 
-        spark = self.builder.getOrCreate()
-
-        return spark
-
     def delta_connector(self):
-        pass
+        self.builder = (
+            self.builder
+            .config('spark.sql.extensions', 'io.delta.sql.DeltaSparkSessionExtension')
+            .config('spark.sql.catalog.spark_catalog', 'org.apache.spark.sql.delta.catalog.DeltaCatalog')
+        )
